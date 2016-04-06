fcbf451d742b5622ebabce6f986be28631d358e4
Charles Holbrow <charlesholbrow@gmail.com>
2016-04-06 17:41:59 -0400

    switch to semi-colo style

--- ks-images/application/static/js/client-app.js
+++ ks-images/application/static/js/client-app.js
@@ -1,29 +1,29 @@
 // sock is a Socket object, 
 // sock.io is a Manager object which represents the connection
 // to the socket.io server. 
-window.sock = io()
+window.sock = io();
 
 
 // Connect, and then join the room in the pathname /score/room
-console.log('Attempting to connect to the socket server')
-sock.connect()
+console.log('Attempting to connect to the socket server');
+sock.connect();
 sock.on('connect', function(){
 
   // By Default, this will be called again if the connection is
   // interrupted (for example if the server crashes)
-  console.log('Connection established!')
+  console.log('Connection established!');
 
   // ['', 'score', 'abc'] <= '/score/abc'
-  var pathParts = window.location.pathname.split('/')
-  sock.emit('join', pathParts[2])
-})
+  var pathParts = window.location.pathname.split('/');
+  sock.emit('join', pathParts[2]);
+});
 
 
 // Let the server tell us when to push a new image
 sock.on('img', function(url){
-  scoreStack && scoreStack.push(url)
-})
+  if (scoreStack) scoreStack.push(url);
+});
 
 window.onload = function(){
-  window.scoreStack = new KS.Stack(5)
-}
+  window.scoreStack = new KS.Stack(5);
+};
--- ks-images/application/static/js/client-ks.js
+++ ks-images/application/static/js/client-ks.js
@@ -1,4 +1,4 @@
-window.KS = {}
+window.KS = {};
 
 
 ////////////////////////////////////////////////////////////////
@@ -6,9 +6,9 @@ window.KS = {}
 // KS.Score
 //
 KS.Score = function(){
-  this.div = document.createElement('div')
-  this.div.className = 'ks-image'
-}
+  this.div = document.createElement('div');
+  this.div.className = 'ks-image';
+};
 
 KS.Score.prototype = {
 
@@ -17,60 +17,60 @@ KS.Score.prototype = {
     if (typeof target !== 'string'){
       return this._src;
     } else {
-      this._src = target
+      this._src = target;
       reqwest({
         url: target,
         success: function(doc){
-          clearChildren(self.div)
-          var svg = qwery('svg', doc)[0]
-          self.svg = svg
-          self.div.appendChild(svg)
-          if (!svg) {return cb && cb(new Error('ajax ok, but response missing svg'))}
-          cb && cb(null, doc)
+          clearChildren(self.div);
+          var svg = qwery('svg', doc)[0];
+          self.svg = svg;
+          self.div.appendChild(svg);
+          if (!svg) {return cb && cb(new Error('ajax ok, but response missing svg'));}
+          if (cb) cb(null, doc);
         },
-        error: function(err){cb && cb(err)}
-      })
+        error: function(err){if (cb)cb(err);}
+      });
     }
   },
 
   opacity: function(amt, duration){
-    duration = Math.floor(duration || 0)
-    this.div.style.transition = 'opacity '+ duration+'ms'
-    this.div.style.opacity = amt
+    duration = Math.floor(duration || 0);
+    this.div.style.transition = 'opacity '+ duration+'ms';
+    this.div.style.opacity = amt;
   },
 
   normalizeStaffPositionOnYAxis: function(){
-    var svgGroups = qwery('g[stroke-linecap="butt"][transform]', this.div)
+    var svgGroups = qwery('g[stroke-linecap="butt"][transform]', this.div);
     var userCoordsOffset =  _.chain(svgGroups).filter(function(group){
       // Get only groups with exactly five elements. We hope
       // that these are staff elements.
-      return (qwery('polyline', group).length === 5)
+      return (qwery('polyline', group).length === 5);
     }).filter(function(group){
       // Get only groups with a valid transform. MuseScore is
       // known to use this typeof of transform when outputting 
       // svg
-      return group.transform.baseVal.getItem(0).type === SVGTransform.SVG_TRANSFORM_MATRIX
+      return group.transform.baseVal.getItem(0).type === SVGTransform.SVG_TRANSFORM_MATRIX;
     }).map(function(group){
-      var baseVal0 = group.transform.baseVal.getItem(0)
-      return baseVal0.matrix.f // this is the y-translate value
-    }).sort().first().value()
+      var baseVal0 = group.transform.baseVal.getItem(0);
+      return baseVal0.matrix.f; // this is the y-translate value
+    }).sort().first().value();
 
     // This makes the rather dangerout assumption that the
     // first group contains everything. Unfortunately, applying
     // transform directly to the svg object, does not work.
-    var firstGroup = qwery('g', this.svg)[0]
-    if (!firstGroup) throw new Error('could not find an svg group')
+    var firstGroup = qwery('g', this.svg)[0];
+    if (!firstGroup) throw new Error('could not find an svg group');
 
     var userCoordsHeightChange = userCoordsOffset * -1;
-    userCoordsHeightChange += 120
+    userCoordsHeightChange += 120;
 
     // create the transform we will be useing 
-    var matrix      = this.svg.createSVGMatrix().translate(0, userCoordsHeightChange)
-      , transform   = this.svg.createSVGTransformFromMatrix(matrix)
+    var matrix      = this.svg.createSVGMatrix().translate(0, userCoordsHeightChange);
+    var transform   = this.svg.createSVGTransformFromMatrix(matrix);
 
-    firstGroup.transform.baseVal.appendItem(transform)
+    firstGroup.transform.baseVal.appendItem(transform);
   }
-}
+};
 
 
 ////////////////////////////////////////////////////////////////
@@ -78,38 +78,38 @@ KS.Score.prototype = {
 // KS.Stack
 //
 KS.Stack = function(size){
-  var parent = document.getElementById('ks-container')
-  this.size = size || 4
-  this.currentIndex = -1
-  this.scores = []
+  var parent = document.getElementById('ks-container');
+  this.size = size || 4;
+  this.currentIndex = -1;
+  this.scores = [];
   for (var i = 0; i < size; i++){
-    var score = new KS.Score()
-    this.scores.push(score)
-    parent.appendChild(score.div)
+    var score = new KS.Score();
+    this.scores.push(score);
+    parent.appendChild(score.div);
   }
-}
+};
 
 KS.Stack.prototype = {
   push: function(target){
-    var oldScore = this.scores[this.currentIndex]
-    this.currentIndex = (this.currentIndex + 1) % this.size
-    var newScore = this.scores[this.currentIndex]
+    var oldScore = this.scores[this.currentIndex];
+    this.currentIndex = (this.currentIndex + 1) % this.size;
+    var newScore = this.scores[this.currentIndex];
 
-    newScore.opacity(0, 0)
+    newScore.opacity(0, 0);
     newScore.src(target,function(err){
       if (err){
-        console.error('failed to get new score:', err)
-        return
+        console.error('failed to get new score:', err);
+        return;
       }
       // Align the new staff with the old staff.
-      var staffOffset = newScore.normalizeStaffPositionOnYAxis()
+      var staffOffset = newScore.normalizeStaffPositionOnYAxis();
       // Crossfade images
-      if (oldScore) {oldScore.opacity(0, 450)}
-      newScore.opacity(1, 300)
-    })
+      if (oldScore) {oldScore.opacity(0, 450);}
+      newScore.opacity(1, 300);
+    });
   }
-}
+};
 
 clearChildren = function(node){
   while (node.firstChild) node.removeChild(node.firstChild);
-}
+};
